d5eb6257a69ce55f32bf832cb0d68c0d3c6cf7aa
Paolo <paololykhotop25@gmail.com>
2024-06-17 12:51:54 +0200

    first commit

---
+++ Res Fortnite.py
@@ -0,0 +1,35 @@
+import os
+
+Directory = "C:/Users/ilros/AppData/Local/FortniteGame/Saved/Config/WindowsClient/GameUserSettings.ini"
+try:
+    def remove_read_only(Directory):
+        permissions = os.stat(Directory).st_mode
+        os.chmod(Directory, permissions | 0o200)
+
+    def set_read_only(Directory):
+        permissions = os.stat(Directory).st_mode
+        os.chmod(Directory, permissions & ~0o200)
+
+    remove_read_only(Directory)
+    print('Read Only is off')
+    
+    with open(Directory, 'r') as file:
+        content = file.read()
+
+    OldResX = int(input('Enter the X value you want to change: '))
+    OldResY = int(input('Enter the Y value you want to change: '))
+    NewResX = int(input('Enter the new X value: '))
+    NewResY = int(input('Enter the new Y value: '))
+
+    new_content = content.replace(f"ResolutionSizeX={OldResX}", f"ResolutionSizeY={OldResY}").replace(f"ResolutionSizeX={NewResX}", f"ResolutionSizeY={NewResY}")
+    
+    with open(Directory, "w") as file:
+        file.write(new_content)
+
+    set_read_only(Directory)
+    print('Read-Only is on')
+
+except FileNotFoundError:
+    print(f"Error: File '{Directory}' does not exist.")
+except IOError:
+    print("Error: A problem occurred while reading or writing the file.")
